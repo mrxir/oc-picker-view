13583e84cf0fe07e27642e2ce1ca1039f16f4c22
我也没有时光机器 <100885521@qq.com>
2017-12-27 20:11:49 +0800

    新方法使用demo完善

--- MRPickerView/ViewController.m
+++ MRPickerView/ViewController.m
@@ -10,37 +10,46 @@
 
 #import "MRPickerView.h"
 
-@interface ViewController ()
-
-- (IBAction)didClickDateSelectButton:(UIButton *)button;
-- (IBAction)didClickYearSelectButton:(UIButton *)button;
-
-@end
-
 @implementation ViewController
 
-- (void)didClickYearSelectButton:(UIButton *)button
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if (indexPath.row == 0) {
+        [self showDatePickerWithDatasourceArray];
+    } else if (indexPath.row == 1) {
+        [self showSingleRangeDatePicker];
+    } else if (indexPath.row == 2) {
+        [self showDoubleRangeDatePicker];
+    }
+}
+
+- (void)showDatePickerWithDatasourceArray
 {
     NSArray *dates = @[@"2013年",
                        @"2016年",
                        @"2048年"];
     
-    [MRPickerView showPickerWithComponents:dates
-                          selectCompletion:^(NSInteger row) {
-                              
-                              NSLog(@"选择了 %d 行", (unsigned)row);
-                              
-                          } confirmCompletion:^(NSInteger row) {
-                              
-                              NSLog(@"确定了 %d 行", (unsigned)row);
-                              
-                          }];
+    [MRPickerView showPickerWithArray:dates
+                     selectCompletion:^(NSInteger row) {
+                         
+                         NSLog(@"选择了 %d 行", (unsigned)row);
+                         
+                     } confirmCompletion:^(NSInteger row) {
+                         
+                         NSLog(@"确定了 %d 行", (unsigned)row);
+                         
+                     }];
     
-    [MRPickerView setSelectedRow:5 animated:YES];
+    [MRPickerView setSelectedRow:1 animated:YES];
 }
 
-- (void)didClickDateSelectButton:(UIButton *)button
+- (void)showSingleRangeDatePicker
 {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy年MM月dd日";
+    
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     dateFormatter.dateFormat = @"yyyy";
     
@@ -50,15 +59,48 @@
     [MRPickerView showPickerWithDateFormatterStyle:MRDateFormatterStyleYearMonthDay
                                        minimumDate:miniDate
                                        maximumDate:maxiDate
-                                  selectCompletion:^(NSDate *date, NSDateFormatter *formatter) {
+                                  selectCompletion:^(NSDate *date) {
                                       
                                       NSLog(@"选中了日期: %@", [formatter stringFromDate:date]);
                                       
-                                  } confirmCompletion:^(NSDate *date, NSDateFormatter *formatter) {
+                                  } confirmCompletion:^(NSDate *date) {
                                       
                                       NSLog(@"确认了日期: %@", [formatter stringFromDate:date]);
                                       
                                   }];
+    
+    [MRPickerView setSelectedDate:[NSDate date] animated:YES];
 }
+
+- (void)showDoubleRangeDatePicker
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd";
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy/MM/dd";
+    
+    NSDate *beginMinimumDate = [dateFormatter dateFromString:@"2017/11/11"];
+    NSDate *beginMaximumDate = [NSDate date];
+    
+    NSDate *endMinimumDate = [dateFormatter dateFromString:@"2017/12/12"];
+    NSDate *endMaximumDate = [dateFormatter dateFromString:@"2017/12/30"];
+    
+    [MRPickerView showPickerWithDateFormatter:formatter
+                             beginMinimumDate:beginMinimumDate
+                             beginMaximumDate:beginMaximumDate
+                               endMinimumDate:endMinimumDate
+                               endMaximumDate:endMaximumDate
+                                       offset:0
+                             selectCompletion:^(NSDate *beginDate, NSDate *endDate, NSInteger offset) {
+                                 NSLog(@"选中了开始日期: %@ 结束日期: %@ 相隔天数: %d", [formatter stringFromDate:beginDate], [formatter stringFromDate:endDate], (unsigned)offset);
+                             } confirmCompletion:^(NSDate *beginDate, NSDate *endDate, NSInteger offset) {
+                                 NSLog(@"确认了开始日期: %@ 结束日期: %@ 相隔天数: %d", [formatter stringFromDate:beginDate], [formatter stringFromDate:endDate], (unsigned)offset);
+                             }];
+    
+    [MRPickerView setSelectedBeginDate:[NSDate date] endDate:[NSDate date] animated:YES];
+}
+
+
 
 @end
